668741ce5b7f17ed84db08e40dd9521ba874cd15
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-03-29 12:45:30 -0400

    Read transactions from DB in Transactions screen

--- src/screens/Transactions/index.js
+++ src/screens/Transactions/index.js
@@ -1,13 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TransactionsList from '../../screens/TransactionsList';
 import TransactionDetail from '../../screens/TransactionDetail';
-import { useTransactions } from '../../context/transactionsContext'
 
 const Stack = createNativeStackNavigator();
 
 export default function Transactions() {
-  const { transactions } = useTransactions();
-  console.log(transactions)
 
   return (
     <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }} >
--- src/screens/TransactionsList/index.js
+++ src/screens/TransactionsList/index.js
@@ -1,9 +1,11 @@
 import { View, FlatList } from 'react-native';
-import { transactionsData } from '../../data/mockData';
 import styles from "./styles";
 import ListItem from '../../components/ListItem';
+import { useTransactions } from '../../context/transactionsContext';
 
 export default function TransactionsList({ navigation }) {
+  const { transactions } = useTransactions();
+
   const renderItem = ({ item }) => (
     <ListItem listItem={item} navigation={navigation} />
   );
@@ -11,7 +13,7 @@ export default function TransactionsList({ navigation }) {
   return (
     <View style={styles.container}>
       <FlatList
-        data={transactionsData}
+        data={transactions}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
       />
